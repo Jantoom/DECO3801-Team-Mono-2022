d1c8b2f187bd5e80cce9d0f7495220a62d8b766a
s.norouzi@uqconnect.edu.au <88065279+ShirinNI@users.noreply.github.com>
2022-08-27 09:23:40 +1000

    BombPlant script added again

---
+++ Assets/Scripts/BombPlant.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPlant : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    private Rigidbody playerRb;
+    private Vector3 spawnPo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.Bomb planted");
+            PlantBomb();
+        }
+
+    }
+
+    //Plant a bomb at player's position
+    void PlantBomb()
+    {
+        spawnPo = playerRb.transform.position;
+        Instantiate(bombPrefab, spawnPo, bombPrefab.transform.rotation);
+
+    }
+}
