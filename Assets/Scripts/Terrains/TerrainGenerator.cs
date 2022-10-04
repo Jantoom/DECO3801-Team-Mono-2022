0abd3b1520eaceb7c8fe2b7b1b774c04c93b811c
jaleel_abdur-raheem@hotmail.com <75770311+Jantoom@users.noreply.github.com>
2022-10-04 13:06:09 +1000

    Added row de/generation. Added spawning and despawning animations for terrain rows. Added basic randomness to chosen terrains. Added camera movement based on generation speed.

--- Assets/Scripts/Terrains/TerrainGenerator.cs
+++ Assets/Scripts/Terrains/TerrainGenerator.cs
@@ -5,40 +5,97 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    private Vector3 rollingRowSpawnPoint = new Vector3(0f, -0.5f, 0f);
-    [SerializeField]
-    private GameObject[] terrainPrefabs;
-    [SerializeField]
-    private RuntimeAnimatorController animController;
-    private GameObject parentObject;
+    public static readonly float GENERATION_DELAY = 2.0f, GENERATION_COOLDOWN = 1.0f, GENERATION_SPEED = 2.0f;
+    public static readonly int NUM_ROWS_PER_TERRAIN = 10, MAX_TERRAINS_PER_MAP = 5, MAX_ACTIVE_ROWS = 12;
+    [SerializeField] private GameObject[] terrainPrefabs;
+    [SerializeField] private RuntimeAnimatorController animController;
+    private GameObject mapObj, cameraObj;
+    private bool isGenerating = false;
+    private Queue<GameObject> activeRows = new();
 
     void Start()
     {
-        parentObject = GameObject.Find("Map");
-        StartCoroutine(SpawnTerrain());
+        mapObj = GameObject.Find("Map");
+        cameraObj = GameObject.Find("Main Camera");
+        StartCoroutine(GenerateMap());
     }
 
-    private IEnumerator SpawnTerrain() {
-        var terrainCount = 0;
-        while (true) {
-            var terrainPrefab = PickNextRandomValidTerrain();
-            for (int i = 0; i < terrainPrefab.transform.childCount; i++) {
-                var rowPrefab = terrainPrefab.transform.GetChild(i).gameObject;
-                var spawnedRow = Instantiate(new GameObject(), parentObject.transform.position + new Vector3(0, -0.5f, i + 10f * terrainCount), parentObject.transform.rotation);
-                spawnedRow.transform.SetParent(parentObject.transform);
-                for (int j = 0; j < rowPrefab.transform.childCount; j++) {
-                    var cellPrefab = rowPrefab.transform.GetChild(j).gameObject;
-                    var spawnedCell = Instantiate(cellPrefab, parentObject.transform.position + new Vector3(j, -0.5f, i + 10f * terrainCount), parentObject.transform.rotation);
-                    spawnedCell.AddComponent<Animator>().runtimeAnimatorController = animController;
-                    spawnedCell.transform.SetParent(spawnedRow.transform);
-                }               
-                yield return new WaitForSeconds(1.0f);
+    void Update() {
+        if (isGenerating) {
+            cameraObj.transform.Translate(Vector3.forward * GENERATION_COOLDOWN * Time.deltaTime, Space.World);
+        }
+    }
+
+    private IEnumerator GenerateMap() {
+        yield return GenerateInitialTerrain();
+        isGenerating = true;
+        yield return GenerateTerrain();
+    }
+
+    private IEnumerator GenerateInitialTerrain() {
+        var terrainPref = PickNextRandomValidTerrain();
+        var terrainObj = new GameObject($"Terrain{0} ({terrainPref.name})");
+        terrainObj.transform.SetParent(mapObj.transform, false);
+        terrainObj.transform.localPosition = Vector3.forward * NUM_ROWS_PER_TERRAIN * 0;
+
+        for (var rowCount = 0; rowCount < NUM_ROWS_PER_TERRAIN; rowCount++) {
+            GenerateRow(terrainPref, terrainObj, rowCount);
+        }
+        yield return new WaitForSeconds(GENERATION_DELAY);
+    }
+
+    private IEnumerator GenerateTerrain() {
+        for (var terrainCount = 1; terrainCount < MAX_TERRAINS_PER_MAP; terrainCount++) {
+            var terrainPref = PickNextRandomValidTerrain();
+            var terrainObj = new GameObject($"Terrain{terrainCount} ({terrainPref.name})");
+            terrainObj.transform.SetParent(mapObj.transform, false);
+            terrainObj.transform.localPosition = Vector3.forward * NUM_ROWS_PER_TERRAIN * terrainCount;
+
+            for (var rowCount = 0; rowCount < NUM_ROWS_PER_TERRAIN; rowCount++) {
+                GenerateRow(terrainPref, terrainObj, rowCount);
+                yield return new WaitForSeconds(GENERATION_COOLDOWN);
             }
-            terrainCount++;
+        }
+    }
+
+    private void GenerateRow(GameObject terrainPref, GameObject terrainObj, int rowCount) {
+        var rowPref = terrainPref.transform.GetChild(rowCount).gameObject;
+        var rowObj = new GameObject($"Row{rowCount}");
+        rowObj.transform.SetParent(terrainObj.transform, false);
+        rowObj.transform.localPosition = Vector3.forward * rowCount;
+
+        for (var cellCount = 0; cellCount < rowPref.transform.childCount; cellCount++) {
+            var cellPref = rowPref.transform.GetChild(cellCount).gameObject;
+            var cellObj = Instantiate(cellPref, rowObj.transform, false);
+            cellObj.name = $"Cell{cellCount}";
+            cellObj.transform.localPosition = Vector3.right * cellCount;
+            // Add extra Animator Controller for load in animation
+            var animator = cellObj.AddComponent<Animator>();
+            animator.runtimeAnimatorController = animController;
+            animator.applyRootMotion = true;
+        }
+        activeRows.Enqueue(rowObj);
+        if (activeRows.Count > MAX_ACTIVE_ROWS) {
+            StartCoroutine(DegenerateRow(activeRows.Dequeue()));
+        }
+    }
+
+    private IEnumerator DegenerateRow(GameObject rowObj) {
+        var terrainObj = rowObj.transform.parent.gameObject;
+
+        for (var cellCount = 0; cellCount < rowObj.transform.childCount; cellCount++) {
+            var cellObj = rowObj.transform.GetChild(cellCount).gameObject;
+            cellObj.GetComponent<Animator>().Play("Despawn");
+        }
+        yield return new WaitForSeconds(GENERATION_SPEED);
+
+        Destroy(rowObj);
+        if (terrainObj.transform.childCount == 0) {
+            Destroy(terrainObj);
         }
     }
 
     private GameObject PickNextRandomValidTerrain() {
-        return terrainPrefabs[0];
+        return terrainPrefabs[Random.Range(0, terrainPrefabs.Length)];
     }
 }
