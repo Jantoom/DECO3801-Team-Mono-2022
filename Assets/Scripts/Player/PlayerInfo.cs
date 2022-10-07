7f3cd62aaa016c3de23d7a8025f846e870016329
jaleel_abdur-raheem@hotmail.com <75770311+Jantoom@users.noreply.github.com>
2022-10-08 02:57:56 +1000

    Fixed heal not working while invincible. Increased gravity in despawn animation for blocks.

--- Assets/Scripts/Player/PlayerInfo.cs
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -8,7 +8,7 @@ public class PlayerInfo : MonoBehaviour
     public GameObject Player { get => gameObject; }
     // Player Stats
     private int health = GameInfo.BASE_HEALTH * 30, attack = GameInfo.BASE_HEALTH * 5;
-    public int Health { get => health; set => health = Invincible && value < 0 ? health : Mathf.Clamp(value, 0, GameInfo.BASE_HEALTH * 30); }
+    public int Health { get => health; set => health = Invincible && value < health ? health : Mathf.Clamp(value, 0, GameInfo.BASE_HEALTH * 30); }
     public int Attack { get => Mathf.RoundToInt(attack * (Juggernaut ? 2.0f : (Exhaust ? 0.2f : 1.0f))); }
     public bool Juggernaut = false, Exhaust = false, Invincible = false;
     // Player Bomb
