880644c8503976aff182521d38ff9e79861351f4
s.norouzi@uqconnect.edu.au <88065279+ShirinNI@users.noreply.github.com>
2022-10-07 13:29:55 +1000

    Added players' lives functionalities. Now player1 lose a life by press"Y" and player2 by"U" Removed button from GameOver screen. Added a new class  "GameOverIfo". It has a static variable to detect if the game is over. Implemented the game over condition (All lives of player are lost or time is over) Changed the time limit to 1 min

---
+++ Assets/Scripts/GameOver/GameOverDisplay.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameOverDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text gameOverText, winnerText, winnerNameText, menuText;
+    [SerializeField]
+    GameObject gameOverScreen;
+    TimeDisplay timeValue;
+    bool displayGameOver;
+    [SerializeField] private KeyCode transitionKey;
+    [SerializeField] private string scene;
+
+    private void Awake()
+    {
+
+        timeValue = GameObject.Find("TimeDisplayManager").GetComponent<TimeDisplay>();
+        displayGameOver = false;
+
+
+    }
+    void Update()
+    {
+        if (GameOverInfo.isGameOver)
+        {
+            if (!displayGameOver)
+            {
+                DisplayGameOverScreen();
+            }
+            if (Input.GetKeyDown(transitionKey))
+            {
+                SceneManager.LoadScene(scene);
+            }
+        }
+
+
+    }
+    public void DisplayGameOverScreen()
+    {
+        Debug.Log(timeValue.IsTimeOver());
+        displayGameOver = true;
+        gameOverScreen.SetActive(true);
+        gameOverText.text = "Game Over";
+        winnerText.text = "Winner is";
+        winnerNameText.text = "Player 1";//?
+        menuText.text = "Sit on any chair to go back to the Main Menu (Press 1)";
+
+        //gameOverButton.SetActive(true);
+        FindObjectOfType<AudioManager>().stop("GameMusic");
+        FindObjectOfType<AudioManager>().play("WinSound");
+    }
+
+
+}
