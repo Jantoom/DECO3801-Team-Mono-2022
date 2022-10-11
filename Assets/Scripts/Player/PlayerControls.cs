d19c98068282be93e5a07e5a55d258458e0dcebf
jaleel_abdur-raheem@hotmail.com <75770311+Jantoom@users.noreply.github.com>
2022-10-11 16:56:51 +1000

    Added cleaner implementation for serial and keyboard inputs, with a toggle exposed to the inspector. Removed bomb and item keys in favour of item activation by pressing left and right quickly together.

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -5,128 +5,137 @@ using System.IO.Ports;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Serial Port Comms from arduino
-    
-    int movement;
-
-    static SerialPort sp = new SerialPort("COM3", 9600);
-
     private PlayerInfo playerInfo;
-    // Player Controls
-    [SerializeField]
-    // Change int to KeyCode and enter keycodes 
-    private KeyCode forwardKey, backKey, leftKey, rightKey, bombKey, itemKey;
-    public KeyCode ForwardKey { get => forwardKey; }
-    public KeyCode BackKey { get => backKey; }
-    public KeyCode LeftKey { get => leftKey; }
-    public KeyCode RightKey { get => rightKey; }
-    public KeyCode BombKey { get => bombKey; }
-    public KeyCode ItemKey { get => itemKey; }
+    // Control Type
+    [field: SerializeField] public bool UseSerialControls { get; private set; } = false;
+    // Serial Controls from Arduino
+    private static SerialPort serialInput = new SerialPort("COM3", 9600);
+    [field: SerializeField] public int ForwardSignal { get; private set; }
+    [field: SerializeField] public int BackSignal { get; private set; }
+    [field: SerializeField] public int LeftSignal { get; private set; }
+    [field: SerializeField] public int RightSignal { get; private set; }
+    [field: SerializeField] public int NoSignal { get; private set; }
+    private int lastSignal;
+    // Key Controls from Keyboard
+    [field: SerializeField] public KeyCode ForwardKey { get; private set; }
+    [field: SerializeField] public KeyCode BackKey { get; private set; }
+    [field: SerializeField] public KeyCode LeftKey { get; private set; }
+    [field: SerializeField] public KeyCode RightKey { get; private set; }
+    private KeyCode lastKeyCode;
     // Player Cooldowns
-    private float timeToMove = 0.1f, timeToRebound = 0.25f;
+    public static float ITEM_WAIT_TIME = 0.8f;
+    private float timeSinceLastInput = 0.0f, timeToMove = 0.1f, timeToRebound = 0.25f;
     public float TimeToMove { get => timeToMove * (playerInfo.Exhausted ? 10 : 1); }
     public float TimeToRebound { get => timeToRebound * (playerInfo.Exhausted ? 10 : 1); }
     // Moving
+    public MoveCode MoveStatus { get; private set; } = MoveCode.STATIONARY;
     private int REBOUND_LAYER;
     private Vector3 origPosition, targetPosition;
     private IEnumerator moveCoroutine;
-    private MoveCode moveStatus = MoveCode.STATIONARY;
-    public MoveCode MoveStatus { get => moveStatus; }
 
-    private void Start()
-    {
-        
-        // Opens port and sets a timeout between reads
-        sp.Open();
-        sp.ReadTimeout = 1;
-    }
     void Awake()
     {
         playerInfo = GetComponent<PlayerInfo>();
         REBOUND_LAYER = LayerMask.NameToLayer("Wall");
+        // Opens port and sets a timeout between reads
+        if (UseSerialControls) {
+            serialInput.Open();
+            serialInput.ReadTimeout = 1;
+        }
     }
 
     void Update()
     {
-        
         if (playerInfo.Frozen) {
             return;
         }
-        // Serial port inputs
-        // COMMENT THIS SECTION IF YOU WANT TO PLAY WITH KEYBOARD
-        /*
-        if (sp.IsOpen)
-        {
-            //print(forwardKey);
-            try
-            {
-                movement = (int) sp.ReadByte();
-                print(movement);
-                
-                if (moveStatus == MoveCode.STATIONARY)
-                {
-                    Vector3 direction = Vector3.zero;
-                    if (movement == forwardKey)
-                    {
-                        //print("hello");
-                        direction = Vector3.forward;
-                    } else if (movement == leftKey)
-                    {
-                        direction = Vector3.left;
-                    } else if (movement == rightKey)
-                    {
-                        direction = Vector3.right;
-                    }
-                    //print(direction);
-                    if (direction != Vector3.zero)
-                    {
-                        moveCoroutine = MovePlayer(direction);
-                        StartCoroutine(moveCoroutine);
-                    }
-                }
-            }
-            catch (System.Exception)
-            {
-
-            }
+        if (UseSerialControls) {
+            ProcessSerialInput();
+        } else {
+            ProcessKeyboardInput();
         }
-        */
+    }
 
-        // UNCOMMENT THIS IF YOU WANT TO PLAY WITH KEYBOARD
-        // also have to change int to keycode in keycodes and assign codes
-        
-        // Keyboard input
-        if (moveStatus == MoveCode.STATIONARY) {
-            if (Input.GetKeyDown(bombKey)) {
-                Instantiate(playerInfo.BombPrefab, transform.position, transform.rotation);
-            }
-            else if (Input.GetKeyDown(itemKey)) {
+    private void ProcessSerialInput() {
+        if (serialInput.BytesToRead > 0) {
+            var signal = serialInput.ReadByte();
+            if (MoveStatus == MoveCode.STATIONARY) {
+                Vector3 direction = Vector3.zero;
+                if (signal == ForwardSignal) {
+                    direction = Vector3.forward;
+                } else if (signal == LeftSignal) {
+                    direction = Vector3.left;
+                } else if (signal == RightSignal) {
+                    direction = Vector3.right;
+                } else if (signal == BackSignal) {
+                    direction = Vector3.back;
+                }
+                if (direction != Vector3.zero) {
+                    moveCoroutine = MovePlayer(direction);
+                    StartCoroutine(moveCoroutine);
+                }
+            } else if (((signal == LeftSignal && lastSignal == RightSignal) ||
+                (signal == RightSignal && lastSignal == LeftSignal)) &&
+                Time.time - timeSinceLastInput < ITEM_WAIT_TIME) {
+                // Item Activation
                 var powerup = playerInfo.LoadedPowerup;
                 if (powerup != null) powerup.Activate();
+
+                if (moveCoroutine != null) {
+                    StopCoroutine(moveCoroutine);
+                    StartCoroutine(ReboundPlayer());
+                }
             }
+            lastSignal = signal;
+            timeSinceLastInput = Time.time;
+        }
+    }
+
+    private void ProcessKeyboardInput() {
+        var downKey = KeyCode.None;
+        if (MoveStatus == MoveCode.STATIONARY) {
             Vector3 direction = Vector3.zero;
-            if (Input.GetKeyDown(forwardKey)) {
+            if (Input.GetKeyDown(ForwardKey)) {
+                downKey = ForwardKey;
                 direction = Vector3.forward;
-            }
-            else if (Input.GetKeyDown(leftKey)) {
+            } else if (Input.GetKeyDown(LeftKey)) {
+                downKey = LeftKey;
                 direction = Vector3.left;
-            }
-            else if (Input.GetKeyDown(backKey)) {
-                direction = Vector3.back;
-            }
-            else if (Input.GetKeyDown(rightKey)) {
+            } else if (Input.GetKeyDown(RightKey)) {
+                downKey = RightKey;
                 direction = Vector3.right;
+            } else if (Input.GetKeyDown(BackKey)) {
+                downKey = BackKey;
+                direction = Vector3.back;
             }
             if (direction != Vector3.zero) {
                 moveCoroutine = MovePlayer(direction);
                 StartCoroutine(moveCoroutine);
             }
+        } else if (((Input.GetKeyDown(LeftKey) && lastKeyCode == RightKey) ||
+            (Input.GetKeyDown(RightKey) && lastKeyCode == LeftKey)) &&
+            Time.time - timeSinceLastInput < ITEM_WAIT_TIME) {
+            // Item Activation
+            Debug.Log("We not stationary yo");
+            var powerup = playerInfo.LoadedPowerup;
+            if (powerup != null) powerup.Activate();
+
+            if (moveCoroutine != null) {
+                StopCoroutine(moveCoroutine);
+                StartCoroutine(ReboundPlayer());
+            }
         }
+        if (Input.GetKeyUp(ForwardKey) || Input.GetKeyUp(LeftKey) || Input.GetKeyUp(RightKey)) {
+            lastKeyCode = KeyCode.None;
+        } else {
+            lastKeyCode = downKey;
+        }
+        timeSinceLastInput = Time.time;        
     }
         
 
     void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.layer.Equals(REBOUND_LAYER) && moveStatus.Equals(MoveCode.MOVING) && !playerInfo.Ghosted) {
+        if (collision.gameObject.layer.Equals(REBOUND_LAYER) && MoveStatus.Equals(MoveCode.MOVING) && !playerInfo.Ghosted) {
             if (!collision.gameObject.TryGetComponent<WallWeak>(out var wall) || wall.Health - playerInfo.Attack > 0) {
                 StopCoroutine(moveCoroutine);
                 StartCoroutine(ReboundPlayer());
@@ -136,7 +145,7 @@ public class PlayerControls : MonoBehaviour
     }
 
     private IEnumerator MovePlayer(Vector3 direction) {
-        moveStatus = MoveCode.MOVING;
+        MoveStatus = MoveCode.MOVING;
         origPosition = transform.position;
         targetPosition = origPosition + direction;
         targetPosition = new Vector3(Mathf.Round(targetPosition.x), Mathf.Round(targetPosition.y), Mathf.Round(targetPosition.z));
@@ -150,11 +159,11 @@ public class PlayerControls : MonoBehaviour
         }
 
         transform.position = targetPosition;
-        moveStatus = MoveCode.STATIONARY;
+        MoveStatus = MoveCode.STATIONARY;
     }
 
     private IEnumerator ReboundPlayer() {
-        moveStatus = MoveCode.REBOUNDING;
+        MoveStatus = MoveCode.REBOUNDING;
         var reboundPosition = transform.position;
 
         float elapsedTime = 0f;
@@ -165,7 +174,8 @@ public class PlayerControls : MonoBehaviour
         }
 
         transform.position = origPosition;
-        moveStatus = MoveCode.STATIONARY;
+        MoveStatus = MoveCode.STATIONARY;
+        moveCoroutine = null;
     }   
 }
 
